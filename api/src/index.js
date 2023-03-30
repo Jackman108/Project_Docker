42863a6311a8e75f55349b78c7bb8f6488e9fd88
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2023-03-30 18:40:11 +0300

    Making Database Queries

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const { connectDb } = require('./helpers/db');
-const { host, port } = require('./configuration')
+const { host, port, db } = require('./configuration');
+const postSchema = new mongoose.Schema({
+    name: String
+});
+const Post = mongoose.model('Post', postSchema)
 
 const app = express();
 
@@ -12,6 +17,21 @@ const startServer = () => {
     app.listen(port, () => {
         console.log(`Started api service on port: ${port}`);
         console.log(`On host ${host}`)
+        console.log(`On database ${db}`)
+
+        /* Post.find(function(err, posts) {
+            if (err) return console.error(err);
+            console.log("posts", posts)
+        })*/
+
+        const silence = new Post({ name: "Silence" });
+        silence.save()
+        .then(function (savedSilence) {
+        console.log("savedSilence", savedSilence);
+        })
+        .catch(function (err) {
+  console.log(err);
+        });
     });
 }
 
